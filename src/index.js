ea2639fcdaf846046062da2e7e80a6b788c21528
Qikilca <mizu20000618@icloud.com>
2022-09-03 13:41:21 +0900

    キー入力のテスト

--- src/index.js
+++ src/index.js
@@ -1,5 +1,21 @@
+
+var list = [];
+
+
+function keypressHandler(event) {
+
+    list.push(event.key);
+    document.getElementById("test").innerHTML = list.join("");
+    
+    return false;
+}
+
+
+
 let test = document.getElementById("test");
 
 test.textContent = "Hello World!";
 
 test.style.color = "red";
+
+document.addEventListener("keypress", keypressHandler);
