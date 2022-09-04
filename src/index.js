79e6044879e40420633fd3470ad9147b94d80656
Qikilca <mizu20000618@icloud.com>
2022-09-04 10:22:36 +0900

    正誤判定を作成

--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 var inputKeyList = [];
 
+var num = 0;
+
 //キー入力を受け付けるテスト
 function keypressHandler(event) {
 
     let inputKey = event.key
+
+    
     inputKeyList.push(inputKey);
+
+    console.log(num);
+    console.log(isKeyCorrectAnswer(inputKey,num));
+
+
+    if(isKeyCorrectAnswer(inputKey,num)){
+        num++;
+    }
+
 
     document.getElementById("anser").textContent = inputKeyList.join("");
 
@@ -12,28 +25,34 @@ function keypressHandler(event) {
 }
 
 
-
+//HTMLに問題をセット
 function setQuestion(question) {
-
     document.getElementById("question").textContent = question;
-
 }
 
+//HTMLにルビをセット
 function setRuby(ruby){
     document.getElementById("ruby").textContent = ruby;
 }
 
-function isKeyCorrectAnswer(inputKeyList) {
-    anser = getAnswer();
-    anser.split("");
 
-    console.log(anser.split(""));
+function isKeyCorrectAnswer(inputKey,number) {
+
+    if (inputKey == getAnswer()[number]) {
+        return true;
+    }
+    else {
+        return false;
+    }
 
 }
 
 function getAnswer() {
-    var answer = "test";
-    return answer;
+    let answer = "asakazinja";
+
+    let split_answer = answer.split("")
+
+    return split_answer;
 }
 
 
@@ -41,4 +60,10 @@ setQuestion("安積疏水");
 setRuby("あさかそすいじんじゃ");
 
 
-document.addEventListener("keypress", keypressHandler);
+
+let split_answer = getAnswer();
+console.log(split_answer);
+
+
+document.addEventListener("keypress", keypressHandler)
+
